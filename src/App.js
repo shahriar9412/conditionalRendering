67373ce90a9332b2aa76b4f4c53723cf868b1899
shahriar-hossain <shahriar9412@gmail.com>
2023-09-19 10:57:53 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import ConditionalRendering from './ConditionalRendering'
+
+export default function App() {
+  return (
+    <div>
+      <ConditionalRendering />
+    </div>
+  )
+}
---
+++ src/ConditionalRendering.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react'
+import HomePage from './HomePage'
+import LoginPage from './LoginPage'
+
+export default class ConditionalRendering extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+            isLoggedIn: false
+        }
+    }
+
+    render() {
+        const { isLoggedIn } = this.state
+        return (
+            <div>
+                {isLoggedIn ? <HomePage /> : <LoginPage />}
+            </div>
+        )
+    }
+}
